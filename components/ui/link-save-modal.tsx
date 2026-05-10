3530317d0ccf0ddcdd119628741f3d25fecbe782
KimByeongHun <kimbh0218@naver.com>
2026-05-10 20:42:46 +0900

    폴더명 수정 모달, 링크명 수정 모달 오류 해결

--- components/ui/link-save-modal.tsx
+++ components/ui/link-save-modal.tsx
@@ -75,6 +75,7 @@ export function LinkSaveModal({
               returnKeyType="done"
               onSubmitEditing={handleSave}
               maxLength={50}
+              autoFocus
             />
           </View>
 
